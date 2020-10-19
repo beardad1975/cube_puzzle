e4e85c6bfce10aaae8acf6a4ac8b5dcf83803c9b
Wen-Hung , Chang <bearada1975@gmail.com>
2020-10-19 10:36:16 +0800

    first commit

---
+++ cube_puzzle.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+import ursina
+import numpy as np
+import cv2
+
+
+
+
+#data holder
+class D:
+    # 
+    state = 
+
+
+app = Ursina()
+
+
+def update():
+    pass
+
+
+def input(key):
+    pass
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
